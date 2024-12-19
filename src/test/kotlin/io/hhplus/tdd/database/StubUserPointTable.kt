a3c21c3394f0b4b265b410fe2b6c8362aaf28bd7
yoon-chaejin <kevin3st@naver.com>
2024-12-19 18:48:49 +0900

    test : Stub 객체를 사용하도록 테스트 코드 변경
    - Stub 클래스 선언
      - 포인트가 0이어야 하는 테스트와 아닌 테스트를 지원하도록 StubUserPointTable 생성
    - Service 단의 책임에 맞춰 테스트 변경
      - 최초 조회, 반복 조회 테스트 삭제
      - `충전 후 금액이 최대 보유 가능 금액보다 큰 경우, 실패한다` 테스트 보완
      - `가진 포인트보다 더 큰 포인트를 사용하려는 경우, 실패한다` 테스트 보완

---
+++ src/test/kotlin/io/hhplus/tdd/database/StubUserPointTable.kt
@@ -0,0 +1,17 @@
+package io.hhplus.tdd.database
+
+import io.hhplus.tdd.point.UserPoint
+
+class StubUserPointTable (
+    var userPoint: UserPoint,
+): UserPointTable() {
+    override fun selectById(id: Long): UserPoint {
+        return userPoint
+    }
+
+    override fun insertOrUpdate(id: Long, amount: Long): UserPoint {
+        userPoint = UserPoint(id = id, point = amount, updateMillis = System.currentTimeMillis())
+        return userPoint
+    }
+
+}
--- src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/PointServiceImplTest.kt
@@ -1,62 +1,61 @@
 package io.hhplus.tdd.point
 
-import io.hhplus.tdd.database.FakePointHistoryTable
-import io.hhplus.tdd.database.FakeUserPointTable
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
+import io.hhplus.tdd.database.StubPointHistoryTable
+import io.hhplus.tdd.database.StubUserPointTable
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class PointServiceImplTest
 {
-    private val userPointTable = FakeUserPointTable()
-    private val pointHistoryTable = FakePointHistoryTable()
-    private val sut: PointServiceImpl = PointServiceImpl(userPointTable, pointHistoryTable)
+    private val pointHistoryTable = StubPointHistoryTable()
 
     private val userId = 1L
 
-    @AfterEach
-    fun tearDown() {
-        userPointTable.clearTable()
-        pointHistoryTable.clearTable()
-    }
+    private val userPointWithZeroPoint = UserPoint(
+        id = userId,
+        point = 0,
+        updateMillis = System.currentTimeMillis(),
+    )
 
-    @Test fun `처음 UserPoint를 조회하는 경우, amount가 0이고 해당 id를 갖는 UserPoint를 반환한다`() {
+    private val userPointWithNonZeroPoint = UserPoint(
+        id = userId,
+        point = (PointServiceImpl.MIN_AMOUNT_PER_CHARGE..PointServiceImpl.MAX_AMOUNT_PER_CHARGE).random(),
+        updateMillis = System.currentTimeMillis(),
+    )
+
+    @Test fun `id로 UserPoint를 조회하는 경우, 해당 id를 갖는 UserPoint를 반환한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
 
         //when
         val userPoint = sut.findUserPointById(userId)
 
         //then
+        assertInstanceOf(UserPoint::class.java, userPoint)
         assertEquals(userId, userPoint.id)
-        assertEquals(0, userPoint.point)
     }
 
-    @Test fun `여러 번 UserPoint를 조회하는 경우, 최초와 동일한 UserPoint를 반환한다`() {
+    @Test fun `userId로 PointHistory를 조회하는 경우, 해당 userId를 갖는 PointHistory List를 반환한다`() {
         //given
-        val userPoint = sut.findUserPointById(userId)
-
-        //when
-        val userPointAfterFirstSelect = sut.findUserPointById(userId)
-
-        //then
-        assertEquals(userPoint.id, userPointAfterFirstSelect.id)
-        assertEquals(userPoint.point, userPointAfterFirstSelect.point)
-        assertEquals(userPoint.updateMillis, userPointAfterFirstSelect.updateMillis)
-    }
-
-    @Test fun `내역이 없는 사용자를 조회하는 경우, 빈 리스트를 반환하다`() {
-        //given
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
 
         //when
         val pointHistories = sut.findPointHistoriesByUserId(userId)
 
         //then
-        assertEquals(0, pointHistories.size)
+        assertInstanceOf(List::class.java, pointHistories)
+        for (pointHistory in pointHistories) {
+            assertInstanceOf(PointHistory::class.java, pointHistory)
+            assertEquals(userId, pointHistory.userId)
+        }
     }
 
     @Test fun `충전하려는 금액이 최소 충전 금액보다 작은 경우, 실패한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
         val amount = PointServiceImpl.MIN_AMOUNT_PER_CHARGE - 1
 
         //when
@@ -69,6 +68,8 @@ class PointServiceImplTest
 
     @Test fun `충전하려는 금액이 최대 충전 금액보다 큰 경우, 실패한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
         val amount = PointServiceImpl.MAX_AMOUNT_PER_CHARGE + 1
 
         //when
@@ -81,6 +82,8 @@ class PointServiceImplTest
 
     @Test fun `충전하려는 금액이 최소 충전 금액과 최대 충전 금액 사이에 있는 경우, 성공한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
         val amount = (PointServiceImpl.MIN_AMOUNT_PER_CHARGE..PointServiceImpl.MAX_AMOUNT_PER_CHARGE).random()
 
         //when
@@ -90,20 +93,24 @@ class PointServiceImplTest
         assertEquals(amount, userPoint.point)
     }
 
-    @Test fun `충전 후 총 포인트가 백만보다 큰 경우, 실패한다`() {
+    @Test fun `충전 후 금액이 최대 보유 가능 금액보다 큰 경우, 실패한다`() {
         //given
-        val userPointAfterFirstCharge = sut.charge(1L, 1_000_000L)
-        assertEquals(1_000_000L, userPointAfterFirstCharge.point)
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
+        val userPoint = sut.findUserPointById(userId)
+
         //when
 
         //then
         assertThrows(Exception::class.java) {
-            sut.charge(1L, 1L)
+            sut.charge(userId, PointServiceImpl.MAX_TOTAL_AMOUNT - userPoint.point + 1)
         }
     }
 
     @Test fun `사용하려는 금액이 최소 사용 금액보다 작은 경우, 실패한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
         val amount = PointServiceImpl.MIN_AMOUNT_PER_USE - 1
 
         //when
@@ -116,6 +123,8 @@ class PointServiceImplTest
 
     @Test fun `사용하려는 금액이 최대 사용 금액보다 큰 경우, 실패한다`() {
         //given
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
         val amount = PointServiceImpl.MAX_AMOUNT_PER_USE + 1
 
         //when
@@ -128,8 +137,10 @@ class PointServiceImplTest
 
     @Test fun `가진 포인트보다 더 큰 포인트를 사용하려는 경우, 실패한다`() {
         //given
-        val amount = (PointServiceImpl.MIN_AMOUNT_PER_USE..PointServiceImpl.MAX_AMOUNT_PER_USE).random()
-        sut.findUserPointById(userId)
+        val userPointTable = StubUserPointTable(userPointWithNonZeroPoint)
+        val sut = PointServiceImpl(userPointTable, pointHistoryTable)
+        val userPoint = sut.findUserPointById(userId)
+        val amount = (userPoint.point + 1..PointServiceImpl.MAX_AMOUNT_PER_USE).random()
 
         //when
 
